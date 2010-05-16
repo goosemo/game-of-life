a20475941d1ce4964067c28e1422165b7851a9fb
Morgan Goose <goose@grumpy.morgan-dyn-o-saur.com>
2010-05-16 07:43:17 -0400

    Found out that a lot of GoL people use a specific format for their stuff.
    So I added in a section to split on how to load the file based on the file
    extenstion. If it ends with gol then its the original straight 01 for bit state
    implemenetation. If it ends in rle it is in the run length encoding format that
    I have found lots of files and patterns for.

--- gol.py
+++ gol.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 import os
 import sys
+import string
 import curses
+import itertools
 
 from optparse import OptionParser
 
@@ -16,6 +18,77 @@ def load_board(filename, board):
     """
     loads the file argument into the board
     """
+    if filename.endswith(".gol"):
+        return _explicit_board(filename, board)
+
+    elif filename.endswith(".rle"):
+        return _rle_board(filename, board)
+
+    else:
+        raise Exception("Do not know board format.")
+
+def _decode(line):
+    """
+    Take encoded string and return expanded form
+    """
+    decoded, counting = "", ""
+
+    for c in list(line):
+        if c in ["b", "o"] and not counting:
+            decoded += c
+
+        elif c in ["b", "o"] and counting:
+            decoded += c*int(counting)
+            counting = ""
+            
+        elif c in string.digits:
+            counting += c
+
+                    
+    return decoded            
+            
+
+def _rle_board(filename, board):
+    """
+    Lets us use the rle format as explained here:
+        http://conwaylife.com/wiki/index.php?title=RLE
+    """
+
+    with open(filename) as rle_file:
+        loaded_rle = rle_file.readlines()
+
+    
+    for line in loaded_rle:
+        if not line.startswith("#"):
+            if line.startswith("x"):
+                if "rule" in line:
+                   x, y, rule = line.split(',')
+                else:
+                   x, y = line.split(',')
+            else:
+                pattern = line.split("$")
+
+    cols = int(x.split("=")[-1])
+    rows = int(y.split("=")[-1])
+
+    row_offset = (len(board) - rows)/2
+    col_offset = (len(board[0]) - cols)/2
+    trans = {'b':0, 'o':1}
+
+    row = 0
+    for line in pattern:
+        col = 0
+        decoded = _decode(line)
+        for char in decoded:
+            if char in ['b', 'o']:
+                board[row+row_offset][col+col_offset] = trans[char]
+            col += 1
+        row += 1
+
+    return board                
+        
+
+def _explicit_board(filename, board):
     with open(filename,'r') as f:
         loaded_file = f.readlines()
 
